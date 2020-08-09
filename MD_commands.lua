d054a0ffef91d5910c325ffd5742d0410cb8e5ae
DevNeru <66133036+DevNeru@users.noreply.github.com>
2020-08-09 18:04:00 +0200

    Update MD_commands.lua

--- MD_commands.lua
+++ MD_commands.lua
@@ -1,7 +1,7 @@
 RegisterCommand("discord", function(source, args, rawCommand) -- Je kan "discord" veranderen naar elk ander command dat je wilt.
-    TriggerEvent("chatMessage", "^1[MildamRP] ^7discord.gg/4eyg5N8") -- Als je wilt kan je het bericht aanpassen.
+    TriggerEvent("chatMessage", "^1[Server] ^7discord.gg/jeserver") -- Als je wilt kan je het bericht aanpassen.
 end)
 
 RegisterCommand("help", function(source, args, rawCommand)
-    TriggerEvent("chatMessage", "^1[MildamRP] ^4/discord")
+    TriggerEvent("chatMessage", "^1[Server] ^4/discord")
 end)
